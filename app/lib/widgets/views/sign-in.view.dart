19ab7e7c7cbae59d5f404011a47466571af7bb94
kelidon <v.shchors@gmail.com>
2020-05-31 00:45:41 +0300

    init ui

---
+++ app/lib/widgets/views/sign-in.view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:line_awesome_icons/line_awesome_icons.dart';
+
+class SignInView extends StatelessWidget {
+  SignInView({Key key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    //final signInModel = Provider.of<SignInModel>(context);
+
+    const TextStyle optionStyle =
+    TextStyle(fontSize: 30);
+
+
+    return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Spacer(flex: 3,),
+            Text('Sign in', style: optionStyle,),
+            Spacer(flex: 2,),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: const EdgeInsets.all(20.0),
+                hintText: 'Login',
+              ),
+            ),
+            Spacer(flex: 1,),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: const EdgeInsets.all(20.0),
+                hintText: 'Password',
+              ),
+            ),
+            Spacer(flex: 5,),
+          ],
+        )
+    );
+  }
+}
