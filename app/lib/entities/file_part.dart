ffdc25bb82bbbffa5fde36a55065bbbfbbf43a90
Artsiom Varabei <captainhorsepower228@gmail.com>
2020-05-31 02:05:22 +0300

    create models

---
+++ app/lib/entities/file_part.dart
@@ -0,0 +1,9 @@
+import 'storable_data.dart';
+
+class FilePart extends StoredData {
+
+  
+  
+  FilePart(String ownerEmail, int size, String checkSum) : super(ownerEmail, size, checkSum);
+
+}
--- app/lib/entities/user.dart
+++ app/lib/entities/user.dart
@@ -1,10 +1,1 @@
-class User {
-  String id;
-  String name;
-  String password;
-  List<User> friends;
-  int wallet;
 
-  User(this.id, this.name, this.password, this.friends,
-      this.wallet);
-}
