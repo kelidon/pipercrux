9f34cd14b8258814b7bc91f9a7e267fbf91963e3
Artsiom Varabei <captainhorsepower228@gmail.com>
2020-05-31 17:31:03 +0300

    remove redundant stuff

--- app/lib/data/data_service.dart
+++ app/lib/data/data_service.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:pipercrux/data/models.dart';
-import 'package:pipercrux/main.dart';
+
+import 'models.dart';
 
 class DataService {
   final db = Firestore.instance;
@@ -10,20 +10,6 @@ class DataService {
     if (_instance == null) _instance = DataService();
 
     return _instance;
-  }
-
-  Stream<User> findOneByUsername(String username) {
-    return db
-        .collection('users')
-        .where('username', isEqualTo: username)
-        .snapshots()
-        .expand((c) => c.documents)
-        .map((d) {
-          log.d(d.data);
-          return d.data
-          ..putIfAbsent('uid', () => d.documentID);
-          })
-        .map((map) => User.fromMap(map));
   }
 
   Future<User> findOne(String username) {
